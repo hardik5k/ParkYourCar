a94396c1d4990c7ce9797d0653958a26943d2c19
Hardik Khandelwal <77318140+hardik5k@users.noreply.github.com>
2021-08-26 09:56:10 +0530

    firebase config

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC1l3LgDNrArPfLLKdwJKKfZ-JgwWL0Ya0",
+    authDomain: "parkyourcar-6fb26.firebaseapp.com",
+    databaseURL: "https://parkyourcar-6fb26-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "parkyourcar-6fb26",
+    storageBucket: "parkyourcar-6fb26.appspot.com",
+    messagingSenderId: "738627502117",
+    appId: "1:738627502117:web:135b3acecd1d73ce493fcc",
+    measurementId: "G-1B2HY9XHBX"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  var db  = firebase.database();
